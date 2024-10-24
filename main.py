dfc84c4b9567beb86768985379a9f2c04cebd987
Tadiwanashe Shangwa <tshng@gmail.com>
2024-10-24 05:13:14 +0200

    adds sample code

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from lib.init_database import createDatabaseDir
+from lib.tables import Table
 
 createDatabaseDir()
+
+class UserTable(Table):
+
+    def __init__(self) -> None:
+        super().__init__(tableName="users")
+        self.initTableFile()
+
+    def columns(self) -> list[str]:
+        return ["id", "username", "email"]
+    
+usersTable = UserTable()
+
+usersTable.insertRow([1, "tadiwanashe", "tshxng@gmail.com"])
